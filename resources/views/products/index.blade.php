7fb100a762021a54afbffe532aeba0ab9cff0efd
Paulo <player_one_117@outlook.com>
2022-08-31 10:43:10 -0500

    mostrando mas campos en la tabla de productos y comprobando si existen productos

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,10 @@
 <x-layout>
+    <x-slot:title>
+        Products
+    </x-slot>
+    @empty ($products)
+        <div class="alert alert-warning">There are not products yet</div>
+    @else
     <h1>List of products</h1>
     <div class="table-responsive">
         <table class="table table-striped">
@@ -7,6 +13,9 @@
                     <th>Id</th>
                     <th>Title</th>
                     <th>Description</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                    <th>Status</th>
                 </tr>
             </thead>
             <tbody>
@@ -15,9 +24,13 @@
                     <td>{{$product->id}}</td>
                     <td>{{$product->title}}</td>
                     <td>{{$product->description}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->stock}}</td>
+                    <td>{{$product->status ? 'Avaible' : 'Unavaible'}}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+    @endempty
 </x-layout>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:title>
+        Product
+    </x-slot>
     <h1>{{$product->title}}</h1>
     <p>{{$product->description}}</p>
 </x-layout>

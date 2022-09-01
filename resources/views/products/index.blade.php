abfd9336f410c37ea2ecca21cbc40a40c96515e3
Paulo <player_one_117@outlook.com>
2022-08-31 20:32:00 -0500

    Agregando actiones para cada producto en index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,39 +1,46 @@
 <x-layout>
     <x-slot:title>
         Products
-    </x-slot>
-    {{--@isset($message)
+        </x-slot>
+        <a class="btn btn-success" href="{{route('products.create')}}">Create product</a>
+        {{--@isset($message)
         <p>{{$message}}</p>
-    @endisset--}}
-    @empty ($products)
+        @endisset--}}
+        @empty ($products)
         <div class="alert alert-warning">There are not products yet</div>
-    @else
-    <h1>List of products</h1>
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead class="thead-light">
-                <tr>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Stock</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $product)
-                <tr>
-                    <td>{{$product->id}}</td>
-                    <td>{{$product->title}}</td>
-                    <td>{{$product->description}}</td>
-                    <td>{{$product->price}}</td>
-                    <td>{{$product->stock}}</td>
-                    <td>{{$product->status ? 'Avaible' : 'Unavaible'}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    @endempty
+        @else
+        <h1>List of products</h1>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                    <tr>
+                        <td>{{$product->id}}</td>
+                        <td>{{$product->title}}</td>
+                        <td>{{$product->description}}</td>
+                        <td>{{$product->price}}</td>
+                        <td>{{$product->stock}}</td>
+                        <td>{{$product->status ? 'Avaible' : 'Unavaible'}}</td>
+                        <td>
+                            <a class="btn btn-link" href="{{route('products.show', ['product' => $product->id])}}">Show</a>
+                            <a class="btn btn-link" href="{{route('products.edit', ['product' => $product->id])}}">Edit</a>
+                            <a class="btn btn-link" href="{{route('products.destroy', ['product' => $product->id])}}">Delete</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @endempty
 </x-layout>

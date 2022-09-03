42a930687e0c5080c794f3be10d2c6b56d90365b
Paulo <player_one_117@outlook.com>
2022-09-03 13:11:15 -0500

    Estructura basica para nuevas tablas de la bd, falta establecer relaciones

--- database/migrations/2022_09_02_205609_create_orders_table.php
+++ database/migrations/2022_09_02_205609_create_orders_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('status')->default('pending');
+            $table->foreignId('customer_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
